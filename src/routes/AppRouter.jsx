78da27a8a6c083397cd217ecd510dfee9d68de8b
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-10-25 18:03:49 -0500

    Creacion de la pagina productos

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from '../Components/Layout/Navbar'
 import Home from '../Components/Home/Home'
+import Products from '../Components/products/Products'
 
 const AppRouter = () => {
     return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
             <Routes>
                 <Route element={<Navbar/>}>
                     <Route path='/' element={<Home/>}></Route>
+                    <Route path='/products' element={<Products/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
