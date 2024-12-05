1e0e8c422f4c61cf2a7cd52db30c5b58ff8952a4
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-12-04 22:43:11 -0500

    Creacion de funcionalidad de eliminar porducto con la cuenta de administrador

--- src/Components/products/Products.jsx
+++ src/Components/products/Products.jsx
@@ -2,9 +2,12 @@ import React, { useEffect } from "react";
 import { useProduct } from "../../zustand/useProducts.js";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../zustand/authUsers.js";
+import { MdDelete } from "react-icons/md";
+import { BiEditAlt } from "react-icons/bi";
 
 const Products = () => {
-  const { productos, functionGet, setProductSelected, functionGetProducts } = useProduct();
+  const { productos, functionGet, setProductSelected, functionGetProducts, removeFromProducts } =
+    useProduct();
 
   const { dataSinIn } = useAuth();
 
@@ -13,14 +16,22 @@ const Products = () => {
   };
 
   useEffect(() => {
-    functionGetProducts();
+    // functionGetProducts()
+    functionGet("list");
   }, []);
+
+  const handleDeleteProduct = (productoId) => {
+    removeFromProducts(productoId);
+  };
 
   return (
     <>
       {dataSinIn?.rol === "ADMIN" && (
         <div className="flex my-5 mr-[11%] justify-end items-center ">
-          <Link to="/add-product" className="px-3 flex items-center rounded-lg text-gray-300 font-medium hover:scale-95 transition-all duration-300 justify-center py-3 bg-primary_color">
+          <Link
+            to="/add-product"
+            className="px-3 flex items-center rounded-lg text-gray-300 font-medium hover:scale-95 transition-all duration-300 justify-center py-3 bg-primary_color"
+          >
             Agregar producto
           </Link>
         </div>
@@ -36,11 +47,18 @@ const Products = () => {
                 className="group cursor-pointer"
                 onClick={() => handleDetailProductId(product)}
               >
+                {dataSinIn?.rol === "ADMIN" && (
+                  <div className="absolute z-50 opacity-0 group-hover:opacity-100 transition-all duration-500 mx-[6.5%] my-[8%] flex gap-5">
+                    <MdDelete onClick={() => handleDeleteProduct(product.productoId)} className="text-2xl text-red-500 hover:text-red-900 transition-all duration-500" />
+                    <BiEditAlt className="text-2xl text-green-400 hover:text-green-800 transition-all duration-500" />
+                  </div>
+                )}
+
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                   <img
                     alt={product.nombre}
                     src={product.imagen_url}
-                    className=" w-full object-contain object-center group-hover:opacity-75"
+                    className=" w-full object-contain object-center group-hover:opacity-15"
                   />
                 </div>
                 <h3 className="mt-4 text-sm text-colo_text">
--- src/zustand/useProducts.js
+++ src/zustand/useProducts.js
@@ -3,140 +3,175 @@ import Swal from "sweetalert2";
 import { create } from "zustand";
 
 export const useProduct = create((set) => ({
-    //Estados iniciales
-    productos: [],
-    cart: [],
-    totalPrice: 0,
-    productSelected: JSON.parse(sessionStorage.getItem("productSelected")) || null,
-    categories: [],
-    loading: false,
-    error: null,
-    selectedCategory: "Ruanas",
-    cantidadComprar: 1,
+  //Estados iniciales
+  productos: [],
+  cart: [],
+  totalPrice: 0,
+  productSelected:
+    JSON.parse(sessionStorage.getItem("productSelected")) || null,
+  categories: [],
+  loading: false,
+  error: null,
+  selectedCategory: "Ruanas",
+  cantidadComprar: 1,
 
-    //Acciones
-    functionGet: async (endpoint) => {
-        const URL_API = "http://localhost:8080/api/v1/categories/";
+  //Acciones
+  functionGet: async (endpoint) => {
+    const URL_API = "http://localhost:8080/api/v1/categories/";
 
-        set({
-            loading: true,
-            error: null,
-        });
+    set({
+      loading: true,
+      error: null,
+    });
 
-        try {
-            const response = await axios.get(`${URL_API}${endpoint}`);
-            set({
-                categories: response.data,
-                // productos: response.data.flatMap((product) => product.products),
-                loading: false,
-            });
-        } catch (error) {
-            set({
-                loading: false,
-                error: error.message,
-            });
-        }
-    },
+    try {
+      const response = await axios.get(`${URL_API}${endpoint}`);
+      set({
+        categories: response.data,
+        productos: response.data.flatMap((product) => product.products),
+        loading: false,
+      });
+    } catch (error) {
+      set({
+        loading: false,
+        error: error.message,
+      });
+    }
+  },
 
-    functionGetProducts: async () => {
-        const URL_API_Products = "http://localhost:8080/api/v1/products/list";
+  functionGetProducts: async () => {
+    const URL_API_Products = "http://localhost:8080/api/v1/products/list";
 
-        set({
-            loading: true,
-            error: null,
-        });
+    set({
+      loading: true,
+      error: null,
+    });
 
-        try {
-            const response = await axios.get(`${URL_API_Products}`);
-            console.log(response.data)
-            set({
-                productos: response.data,
-                loading: false,
-            });
-        } catch (error) {
-            set({
-                loading: false,
-                error: error.message,
-            });
-        }
-    },
-    addProduct: async(productData) => {
-        set({loading: true, error: null});
+    try {
+      const response = await axios.get(`${URL_API_Products}`);
+      console.log(response.data);
+      set({
+        productos: response.data,
+        loading: false,
+      });
+    } catch (error) {
+      set({
+        loading: false,
+        error: error.message,
+      });
+    }
+  },
+  addProduct: async (productData) => {
+    set({ loading: true, error: null });
 
-        const URL = "http://localhost:8080/api/v1/products/save"
-        try {
-            const response = await axios.post(`${URL}`, productData);
-            console.log("respuesta de post", response.data);
+    const URL = "http://localhost:8080/api/v1/products/save";
+    try {
+      const response = await axios.post(`${URL}`, productData);
+      console.log("respuesta de post", response.data);
 
-            set((state) => ({
-                productos: [...state.productos, response.data],
-                loading: false,
-            }))
-        } catch (error) {
-            set({
-                error: error.message,
-                loading: false,
-            })
-        }
-    },
+      set((state) => ({
+        productos: [...state.productos, response.data],
+        loading: false,
+      }));
+    } catch (error) {
+      set({
+        error: error.message,
+        loading: false,
+      });
+    }
+  },
 
-    setCategory: (category) => set({ selectedCategory: category }),
+  setCategory: (category) => set({ selectedCategory: category }),
 
-    setProductSelected: (product) => {
-        set({ productSelected: product });
-        sessionStorage.setItem("productSelected", JSON.stringify(product));
-    },
-    setCantidadComprar: (cantidad) => {
-        // sessionStorage.removeItem("cart");
-        set({ 
-            cantidadComprar: cantidad,
-            // cart: []
-        })
-    },
+  setProductSelected: (product) => {
+    set({ productSelected: product });
+    sessionStorage.setItem("productSelected", JSON.stringify(product));
+  },
+  setCantidadComprar: (cantidad) => {
+    // sessionStorage.removeItem("cart");
+    set({
+      cantidadComprar: cantidad,
+      // cart: []
+    });
+  },
 
-    setPriceTotal: (price) => set({ totalPrice: price }),
+  setPriceTotal: (price) => set({ totalPrice: price }),
 
-    addCart: (product, cantidadSeleccionada) => {
+  addCart: (product, cantidadSeleccionada) => {
+    set((state) => {
+      const existingProduct = state.cart.find(
+        (item) => item.productoId === product.productoId
+      );
+
+      const updatedCart = existingProduct
+        ? state.cart.map((item) =>
+            item.productoId === product.productoId
+              ? { ...item, quantity: item.quantity + cantidadSeleccionada }
+              : item
+          )
+        : [...state.cart, { ...product, quantity: cantidadSeleccionada }];
+      Swal.fire({
+        title: "Good job!",
+        text: "Agregado al carrito correctamente!",
+        icon: "success",
+      });
+
+      // Guarda el carrito actualizado en localStorage
+      sessionStorage.setItem("cart", JSON.stringify(updatedCart));
+
+      return { cart: updatedCart };
+    });
+  },
+
+  loadCart: () => {
+    const storedCart = JSON.parse(sessionStorage.getItem("cart") || "[]");
+    set({ cart: storedCart });
+  },
+
+  removeFromCart: (productId) =>
+    set((state) => {
+      const updateCart = state.cart.filter(
+        (item) => item.productoId !== productId
+      );
+
+      sessionStorage.setItem("cart", JSON.stringify(updateCart));
+      return { cart: updateCart };
+    }),
+
+  clearCart: () => {
+    sessionStorage.removeItem("cart");
+    set({ cart: [] }); // Limpia el carrito
+  },
+
+//   removeFromProducts: (productId) =>
+//     set((state) => {
+//       const updateProducts = state.productos.filter(
+//         (item) => item.productoId !== productId
+//       );
+
+//       return { productos: updateProducts };
+//     }),
+
+  removeFromProducts: (productId) => {
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "Este producto será eliminado de la lista.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, eliminar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
         set((state) => {
-            const existingProduct = state.cart.find(
-                (item) => item.productoId === product.productoId
-            );
-
-            const updatedCart = existingProduct
-                ? state.cart.map((item) =>
-                    item.productoId === product.productoId
-                        ? { ...item, quantity: item.quantity + cantidadSeleccionada }
-                        : item
-                )
-                : [...state.cart, { ...product, quantity: cantidadSeleccionada }];
-                Swal.fire({
-                    title: "Good job!",
-                    text: "Agregado al carrito correctamente!",
-                    icon: "success"
-                  });
-
-            // Guarda el carrito actualizado en localStorage
-            sessionStorage.setItem("cart", JSON.stringify(updatedCart));
-
-            return { cart: updatedCart };
+          const updatedProducts = state.productos.filter(
+            (item) => item.productoId !== productId
+          );
+          return { productos: updatedProducts };
         });
-    },
-
-    loadCart: () => {
-        const storedCart = JSON.parse(sessionStorage.getItem("cart") || "[]");
-        set({ cart: storedCart });
-    },
-
-    removeFromCart: (productId) =>
-        set((state) => {
-            const updateCart = state.cart.filter((item) => item.productoId !== productId)
-
-            sessionStorage.setItem("cart", JSON.stringify(updateCart));
-            return { cart: updateCart };
-        }),
-
-    clearCart: () => {
-        sessionStorage.removeItem("cart")
-        set({ cart: [] })// Limpia el carrito
-  }
+        Swal.fire("Eliminado", "El producto ha sido eliminado.", "success");
+      }
+    });
+  },
 }));
